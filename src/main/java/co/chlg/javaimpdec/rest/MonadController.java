cdc619ad8e3691e804d291e7ff9a7d083654632d
Frank Cardona <frankdcm+git@gmail.com>
2018-02-22 16:00:13 -0500

    added MonadControllerTest for v2

--- src/main/java/co/chlg/javaimpdec/rest/MonadController.java
+++ src/main/java/co/chlg/javaimpdec/rest/MonadController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/monad")
 public class MonadController {
 
-  @GetMapping("/map-age/:name")
-  private Map<String, String> getMappingNameAge(@PathVariable("name") String name) {
+  @GetMapping("/map-fibonacci/{num}")
+  private Map<Integer, Integer> getMappingNumFib(@PathVariable("num") int num) {
+    // Pair.of(0, 1) Pair.of(1, 1) Pair.of(1, 2) Pair.of(2, 3) Pair.of(3, 5) //fib(5)==5
     return null;
   }
 
